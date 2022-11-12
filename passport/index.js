c68f1efa9f298da46bfcf969979001d13032e958
april4411 <shlee9605@naver.com>
2022-11-12 17:39:10 +0900

    delete for ignore

--- passport/index.js
+++ passport/index.js
@@ -1,8 +1,8 @@
-const passport = require('passport');
+const passport = require('passport');       //cookie-session passport
 const local = require('./localStrategy');
 const User = require('../models/user');
 
-module.exports=()=>{
+module.exports=()=>{                        //passport userid
     passport.serializeUser((user, done)=>{
         done(null, user.id);
     });
@@ -10,15 +10,6 @@ module.exports=()=>{
     passport.deserializeUser((id, done)=>{
         User.findOne({
             where: {id}
-            // ,include: [{
-            //     model: User,
-            //     attributes: ['id', 'nick'],
-            //     as: 'Followers',
-            // },{
-            //     model: User,
-            //     attributes: ['id', 'nick'],
-            //     as: 'Followings',
-            // }],
         })
             .then(user => done(null, user))
             .catch(err => done(err));
