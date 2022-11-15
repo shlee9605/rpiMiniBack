b82cc4a81143ec28047174c3bd61d22109a7bfb0
shlee9605 <40204622+shlee9605@users.noreply.github.com>
2022-11-15 17:08:59 +0900

    passport v3

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,11 +1,11 @@
 const passport= require('passport');
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
 module.exports=()=>{
-    passport.use(new localStrategy({    //passport field must be accurate
+    passport.use(new LocalStrategy({    //passport field must be accurate
         usernameField: 'userid',
         passwordField: 'password',
     }, async(userid, password, done)=>{ 
